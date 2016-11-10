798683cdc469c0e45953000a7058783b6f50796d
Pavita Jangprasit <pj@twentysix.co.th>
2016-11-11 00:14:51 +0700

    Add matchHeight

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by pavitajangprasit on 24/5/2016.
  */
+// Instantiate EasyZoom instances
+var $easyzoom = $('.easyzoom').easyZoom();
+
+// Get an instance API
+var api = $easyzoom.data('easyZoom');
